5b805702b0e7d0af3c2087f0b2a27114ff2f12dc
Chris Martin <cmartin616@gmail.com>
2014-10-25 18:51:45 -0400

    Added arcGIS bundle and configured paths.  Adjusted size of scenes to iPad.  Added assets folder and test image.

---
+++ MNsolar/menuView.h
@@ -0,0 +1,13 @@
+//
+//  menuView.h
+//  MNsolar
+//
+//  Created by Chris Martin on 10/25/14.
+//  Copyright (c) 2014 Chris Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface menuView : UIViewController
+
+@end
