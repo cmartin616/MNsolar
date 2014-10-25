60c210b41b1ae0b355a6fc6534b1d202da8839c8
Chris Martin <cmartin616@gmail.com>
2014-10-25 18:05:05 -0400

    Initial Commit

---
+++ MNsolar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MNsolar
+//
+//  Created by Chris Martin on 10/25/14.
+//  Copyright (c) 2014 Chris Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
