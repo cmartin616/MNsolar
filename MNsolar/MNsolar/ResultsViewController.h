631fb00f8219b3321d8404e7840066f2e25e0f86
Kathryn Menk <menkx027@umn.edu>
2014-10-27 23:00:24 -0500

    skeleton?

---
+++ MNsolar/MNsolar/ResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.h
+//  MNsolar
+//
+//  Created by Kathryn Menk on 10/27/14.
+//  Copyright (c) 2014 Chris Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UIViewController
+
+@end
